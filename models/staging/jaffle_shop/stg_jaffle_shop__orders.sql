3761b9935e0b3dc4f461fdf6793672f6818b2cf6
Gokhul Mattakara Gopinathan <gokhul.gopinathan@gmail.com>
2025-06-10 21:41:30 +0000

    code refactored

--- models/staging/jaffle_shop/stg_jaffle_shop__orders.sql
+++ models/staging/jaffle_shop/stg_jaffle_shop__orders.sql
@@ -1,7 +1,23 @@
-select
+with source as (
+
+    select * from {{ source('jaffle_shop', 'orders') }}
+
+),
+
+transformed as (
+
+    select 
         id as order_id,
         user_id as customer_id,
         order_date,
-        status
+        status as order_status,
+        row_number() over (
+            partition by user_id 
+            order by order_date, id
+        ) as user_order_seq
 
-from {{ source('jaffle_shop', 'orders') }}
+    from source   
+
+)
+
+select * from transformed
